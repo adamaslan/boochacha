f1b9d99c1c58b4bc7b6534438ff718a4409bfe69
Adam <chillcoders@gmail.com>
2021-10-09 18:42:02 -0400

    hi

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,35 +1,35 @@
-import { createI18n } from "vue-i18n"
-
+import { createI18n } from "vue-i18n";
+//hii
 export function loadLanguages() {
-    const context = import.meta.globEager("./languages/*.ts");
+  const context = import.meta.globEager("./languages/*.ts");
 
-    const languages: AnyObject = {};
+  const languages: AnyObject = {};
 
-    let langs = Object.keys(context);
-    for (let key of langs) {
-        if (key === "./index.ts") return;
-        let lang = context[key].lang;
-        let name = key.replace(/(\.\/languages\/|\.ts)/g, '');
-        // try {
-        //     if (name === "en") console.log('?????')
-        //     // const elLang = await import(`element-plus/lib/locale/lang/en`) as  AnyObject;
-        //     let elLang = await import(`element-plus/lib/locale/lang/${name}`) as AnyObject;
-        //     lang = Object.assign(lang, {el: elLang.deafault.default.el})
-        // } catch (error) {}
-        languages[name] = lang
-    }
-    
-    return languages
+  let langs = Object.keys(context);
+  for (let key of langs) {
+    if (key === "./index.ts") return;
+    let lang = context[key].lang;
+    let name = key.replace(/(\.\/languages\/|\.ts)/g, "");
+    // try {
+    //     if (name === "en") console.log('?????')
+    //     // const elLang = await import(`element-plus/lib/locale/lang/en`) as  AnyObject;
+    //     let elLang = await import(`element-plus/lib/locale/lang/${name}`) as AnyObject;
+    //     lang = Object.assign(lang, {el: elLang.deafault.default.el})
+    // } catch (error) {}
+    languages[name] = lang;
+  }
+
+  return languages;
 }
 
 export const i18n = createI18n({
-    locale: 'zh-cn',
-    fallbackLocale: 'zh-cn',
-    messages: loadLanguages()
-})
+  locale: "zh-cn",
+  fallbackLocale: "zh-cn",
+  messages: loadLanguages(),
+});
 
-export const i18nt = i18n.global.t
+export const i18nt = i18n.global.t;
 
 export function setLanguage(locale: string) {
-    i18n.global.locale = locale
+  i18n.global.locale = locale;
 }
